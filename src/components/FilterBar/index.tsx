77bd1d15874b074c9a757d6e6f7b35894635f99b
Salomão Santana <salomao.nascimento@muralis.com.br>
2023-05-19 16:10:22 -0300

    added filter bar

---
+++ src/components/FilterBar/index.tsx
@@ -0,0 +1,6 @@
+import { Container } from "./styles";
+function FilterBar() {
+  return <Container>FILTERS</Container>;
+}
+
+export default FilterBar;
---
+++ src/components/FilterBar/styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@configs/theme.config";
+
+export const Container = styled("div", {
+  maxWidth: "100%",
+  backgroundColor: "$white",
+  borderRadius: "$radii$3",
+  padding: "$space$3",
+  margin: "$2",
+  boxSizing: "border-box",
+});
